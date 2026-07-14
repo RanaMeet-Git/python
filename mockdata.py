5dac7a15289a881d6da1cc46aac4d3dd094026d1
RanaMeet-Git <rana.meet@fissionlabs.com>
2026-07-14 15:38:21 +0530

    add fast api files

---
+++ mockdata.py
@@ -0,0 +1,15 @@
+products= [
+    {
+        'id':1,
+        'product':"iphone 15",
+        "price":68800,
+        "count":10
+    },
+
+    {
+        'id':2,
+        'product':"samsung s25",
+        "price":72000,
+        "count":10
+    }
+]
